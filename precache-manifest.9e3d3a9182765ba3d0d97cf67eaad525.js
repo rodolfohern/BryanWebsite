077ab20d199f346bfbe62f67cc5ab2a644ba6b7f
Rodolfo Hernandez <DOOMsday@MacBook-Pro.local>
2022-05-13 15:51:46 -0700

    Updates

---
+++ precache-manifest.9e3d3a9182765ba3d0d97cf67eaad525.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da5e72987a0787ddb720",
+    "url": "/BryanWebsite/static/js/main.da5e7298.chunk.js"
+  },
+  {
+    "revision": "a9907cb450f2a34d3b60",
+    "url": "/BryanWebsite/static/js/1.a9907cb4.chunk.js"
+  },
+  {
+    "revision": "6634cde5a94db2655943",
+    "url": "/BryanWebsite/static/js/runtime~main.6634cde5.js"
+  },
+  {
+    "revision": "5a2958427491aa04357904301586dfec",
+    "url": "/BryanWebsite/static/media/babyBryan.5a295842.png"
+  },
+  {
+    "revision": "7cb17c585ffddefc8bee41f83ff6e503",
+    "url": "/BryanWebsite/index.html"
+  }
+];
