7771d2ca3d82cf7bb16cf4e17cb700e1e54eec84
Rodolfo Hernandez <DOOMsday@MacBook-Pro.local>
2022-05-17 15:59:38 -0700

    updated Cart.jsx to be able to add and remove items from cart and update subtotal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+//due to update, switch tag has been changed to Routes
 const App = () => {
   const [ products, setProducts ] =useState([]);
   const [cart, setCart] = useState({});
@@ -17,10 +19,27 @@ const App = () => {
   }
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
+    const cart = await commerce.cart.add(productId, quantity);
 
-    setCart(item.cart);
+    setCart(cart);
   }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+
+    setCart(cart);
+  }
+
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+
+    setCart(cart);
+  }
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+  }
+
   useEffect(() => {
     fetchProducts();
     fetchCart();
@@ -28,12 +47,22 @@ const App = () => {
 
   console.log(cart);
   return (
-    <div>
-      <Navbar totalItems={cart.total_items}/>
-      {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
-      <Cart cart={cart} />
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items}/>
+        <Routes>
+          //if under main page, render products component, this was updated from original Switch method
+          <Route exact path="/" element={<Products products={products} onAddToCart={handleAddToCart} />}/>
+            //if under cart path, then render cart component, this was updated from original Switch method
+          <Route exact path="/cart" element={<Cart 
+          cart={cart} 
+            handleUpdateCartQty={handleUpdateCartQty}
+            handleRemoveFromCart={handleRemoveFromCart}
+            handleEmptyCart={handleEmptyCart}/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
